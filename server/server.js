9596588c793dc1334c9cc46d4356fee11f198688
Nguyen Dinh <nguyenhpdinh@gmail.com>
2022-03-08 02:24:09 -0800

    set port 8000 and set up homepath for port 8000, cloned /build/

--- server/server.js
+++ server/server.js
@@ -5,18 +5,25 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const app = express();
+const path = require('path')
+const port = process.env.PORT || 8000
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000"
+    origin: "http://localhost:8000"
 }))
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'build')))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
 
 require("./config/mongoose.config");
 require("./routes/user.routes")(app);
 require("./routes/poll.routes")(app);
 require("./routes/rating.routes")(app);
 
-app.listen(process.env.MY_PORT, () => console.log(`You are connected to port ${process.env.MY_PORT}`))
+app.listen(port, () => console.log(`You are connected to port ${port}`))
